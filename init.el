bc2360063828fafb4a80b0e813b1f12da5f2a536
Sebastian Wålinder <s.walinder@gmail.com>
2018-08-10 19:35:09 +0200

    Added new modeline, paper-mode, compile config prompt, new work/eye/up timer, etc

--- init.el
+++ init.el
@@ -1,16 +1,49 @@
-(defvar my/config-location (expand-file-name "~/.emacs.d/config.org"))
+;; Get use-package
+(require 'package)
+(setq package-enable-at-startup nil)
 
-(defvar my/config-exported-location (expand-file-name "~/.emacs.d/config.el"))
-(if(file-exists-p my/config-exported-location)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq use-package-always-ensure t)
+
+;; Load config.org
+(defvar my/config-location (expand-file-name (concat user-emacs-directory "config.org")))
+(defvar my/config-exported-location (expand-file-name (concat user-emacs-directory "config.el")))
+(defvar my/config-compiled-location (expand-file-name (concat user-emacs-directory "config.elc")))
+
+(require 'ob-tangle)
+
+(defun my/compare-last-file-update (isOlder isYounger)
+  (interactive)
+  (time-less-p (nth 5 (file-attributes isOlder)) (nth 5 (file-attributes isYounger))))
+
+(defun my/load-compiled-or-raw ()
+  (interactive)
+  (if (and (file-exists-p my/config-compiled-location) (my/compare-last-file-update my/config-location my/config-compiled-location))
+      (load-file my/config-compiled-location)
+    (if (y-or-n-p "Byte compile config? ")
+	(progn
+	  (byte-compile-file my/config-exported-location t))
+      (load-file my/config-exported-location))))
+
+(if (file-exists-p my/config-exported-location)
     (progn
       ;; Export config if it has changed since last export
-      (if (time-less-p (nth 5 (file-attributes my/config-exported-location)) (nth 5 (file-attributes my/config-location)))
+      (if (my/compare-last-file-update my/config-exported-location my/config-location)
 	  (progn
-	    (org-babel-load-file my/config-location)
-	    (message "Config.el updated!"))
+	    (org-babel-tangle-file my/config-location my/config-exported-location "emacs-lisp")
+	    (message "Config.el updated!")
+	    (my/load-compiled-or-raw))
 	(progn
-	  (load-file my/config-exported-location)
-	  (message "Config.el not updated"))))
-
-  (org-babel-load-file my/config-location)
-  (message "Config.el created!"))
+	  (message "Config.el matches config.org")
+	  (my/load-compiled-or-raw))))
+  (org-babel-tangle-file my/config-location my/config-exported-location "emacs-lisp")
+  (message "Config.el created!")
+  (my/load-compiled-or-raw))
