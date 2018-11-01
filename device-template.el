667fe3b704f095eb480122db56c1bca74241bdf9
Sebastian Wålinder <s.walinder@gmail.com>
2018-11-02 00:08:06 +0100

    Added ivy-youtube, device specific config, fixed eww buffer name

---
+++ device-template.el
@@ -0,0 +1,6 @@
+;;(defvar my/device/resolution '(2560 1440))
+(defvar my/device/resolution '(1920 1080))
+
+;;(defvar my/device/monitor-setup '(1 "DVI-D-1" 0 "DP-1" 2 "DVI-I-1"))
+;; Get from doing "xrandr" in shell
+(defvar my/device/monitor-setup nil)
