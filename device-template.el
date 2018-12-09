a41eda20b2871c0124c7bb6a376e96c3c41c788d
Sebastian Wålinder <s.walinder@gmail.com>
2018-12-09 02:24:23 +0100

    Added mu4e

--- device-template.el
+++ device-template.el
@@ -4,3 +4,5 @@
 ;;(defvar my/device/monitor-setup '(1 "DVI-D-1" 0 "DP-1" 2 "DVI-I-1"))
 ;; Get from doing "xrandr" in shell
 (defvar my/device/monitor-setup nil)
+
+(defvar my/device/maildir (concat user-emacs-directory "Maildir"))
